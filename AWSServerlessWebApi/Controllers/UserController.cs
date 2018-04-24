37d7c7d88b4cef996788c288634ada1e85e79a32
Jan Lingat <ajlingat@gmail.com>
2018-04-24 14:09:07 -0700

    uservm and userrepo
    added user repo, user vm, user controller

--- AWSServerlessWebApi/Controllers/UserController.cs
+++ AWSServerlessWebApi/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AWSServerlessWebApi.Models;
 using AWSServerlessWebApi.Repositories;
+using AWSServerlessWebApi.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,8 +17,68 @@ namespace AWSServerlessWebApi.Controllers
 
         public UserController(KORE_Interactive_MSCRMContext context)
         {
-            
+            userRepo = new UserRepo(context);
         }
 
+        [HttpPost]
+        [Route("Create")]
+        public bool Create(string Email, string Password, string FirstName, string LastName)
+        {
+            UserVM user = new UserVM()
+            {
+                Email = Email,
+                Password = Password,
+                FirstName = FirstName,
+                LastName = LastName
+            };
+
+            userRepo.CreateUser(user);
+            return true;
+        }
+
+        [HttpGet]
+        [Route("List")]
+        public IActionResult List()
+        {
+            return new OkObjectResult(userRepo.GetAllUsers());
+        }
+
+        [HttpGet]
+        [Route("GetOneUser/{id}")]
+        public IActionResult GetOneUser(string id)
+        {
+            Guid guid_id = Guid.Parse(id);
+
+            return new OkObjectResult(userRepo.GetOneUser(guid_id));
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public bool Update(string Id, string Email, string Password, string FirstName, string LastName)
+        {
+            Guid guid_id = Guid.Parse(Id);
+
+            UserVM user = new UserVM()
+            {
+                UserId = guid_id,
+                Email = Email,
+                Password = Password,
+                FirstName = FirstName,
+                LastName = LastName
+            };
+
+            userRepo.UpdateOneUser(user);
+            return true;
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public bool Delete(string id)
+        {
+            Guid guid_id = Guid.Parse(id);
+
+            userRepo.DeleteOneUser(guid_id);
+            return true;
+        }
     }
 }
