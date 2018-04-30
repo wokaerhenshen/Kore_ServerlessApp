dd5187799fdbafe3b7f653eb004ee006115106dd
Jan Lingat <ajlingat@gmail.com>
2018-04-30 13:26:29 -0700

    CustomDay_WBI repo / VM
    created customday_wbi repo and vm

---
+++ AWSServerlessWebApi/Repositories/CustomDay_WBIRepo.cs
@@ -0,0 +1,21 @@
+﻿using AWSServerlessWebApi.Models;
+using AWSServerlessWebApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessWebApi.Repositories
+{
+    public class CustomDay_WBIRepo
+    {
+        KORE_Interactive_MSCRMContext _context;
+
+        public CustomDay_WBIRepo(KORE_Interactive_MSCRMContext context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
---
+++ AWSServerlessWebApi/ViewModels/CustomDay_WBIVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerlessWebApi.ViewModels
+{
+    public class CustomDay_WBIVM
+    {
+        public string WBI_Id { get; set; }
+        public string CustomDayId { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
